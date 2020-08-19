98e1e1e25aac82253804ae0957785169e6a9df92
Constantin-dot <konstantinkspb@gmail.com>
2020-08-19 12:21:38 +0300

    Updates

---
+++ precache-manifest.7d2c76d1af33114b1b76c34d7629240b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ffc0093deb52874b06198b31c4ec19",
+    "url": "/Counters/index.html"
+  },
+  {
+    "revision": "b9dcbd5ac7149e9b5dc3",
+    "url": "/Counters/static/css/main.77b6f7e4.chunk.css"
+  },
+  {
+    "revision": "458641aca8fa2b8c2628",
+    "url": "/Counters/static/js/2.b3b76933.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Counters/static/js/2.b3b76933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9dcbd5ac7149e9b5dc3",
+    "url": "/Counters/static/js/main.8f2fe7a5.chunk.js"
+  },
+  {
+    "revision": "42142acae226ab41705c",
+    "url": "/Counters/static/js/runtime-main.f80d99b5.js"
+  }
+]);
